2a0a294a3db11e8ea67fc75561d686b0be7a5c30
RomAFCI <adnot.rom@gmail.com>
2025-05-27 11:36:52 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-tuto/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-tuto/js.js
@@ -2,4 +2,42 @@ let age = 30;
 
 const ageminimum = 18;
 
-console.log(age < ageminimum);
+console.log(age >= ageminimum);
+
+// document.querySelector("p").style.backgroundColor("red");
+
+// document.querySelector("p").style.backgroundColor = "red";
+
+
+// let plus = "+";
+
+// let moins = "-";
+
+// let multiplier = "*";
+
+// let diviser = "/";
+
+
+let cinq =5;
+let trois =3;
+let résultat;
+
+résultat = cinq + trois;
+
+console.log(résultat);
+
+
+let dix =10;
+let sept =7;
+
+résultat = dix - sept;
+
+console.log(résultat);
+
+let quatre =4;
+let six =6;
+
+résultat = quatre * six;
+
+console.log(résultat);
+
