d81243a469356f97015fbce677bd23a0573fbc88
RomAFCI <adnot.rom@gmail.com>
2025-05-27 15:48:27 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-tuto/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-tuto/js.js
@@ -20,6 +20,7 @@ console.log(age >= ageminimum);
 
 let cinq =5;
 let trois =3;
+
 let résultat;
 
 résultat = cinq + trois;
@@ -41,3 +42,70 @@ résultat = quatre * six;
 
 console.log(résultat);
 
+let deux = 2;
+
+résultat = 10 % 7;
+
+console.log(résultat);
+
+résultat = sept ** deux;
+
+console.log(résultat);
+
+let text = "Bonjour";
+
+text = text + " je m'apelle Romain";
+
+console.log(text);
+
+let prenom = "John";
+
+let nom = " Doe";
+
+texte = prenom + nom; 
+
+console.log(texte);
+
+let sujet = `Le chat`;
+
+let verbe = " mange";
+
+let objet = " la souris";
+
+texte = sujet + verbe + objet;
+
+console.log(texte);
+
+// FONCTION
+
+let bjr = `Bonjour, `
+
+function saluer (a){
+
+ return (bjr + a)
+
+}
+
+console.log(saluer("Alice !"))
+
+let nombre7 = 7;
+let nombre8 = 8;
+
+function multiplier (a, b){
+    return a * b
+}
+
+let resultat = multiplier(7,8);
+console.log ("Résultats de l'addition :", resultat);
+
+// TABLEAU
+
+let tableau = [1, 2, 3, 4, 5];
+
+console.log(tableau);
+
+let tab = [
+    push(10)
+]
+
+console.log(tab);
