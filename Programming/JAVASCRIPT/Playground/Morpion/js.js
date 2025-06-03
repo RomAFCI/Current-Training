ac21034df27f9ca568be26ec257f07c9e9f06c99
RomAFCI <adnot.rom@gmail.com>
2025-06-03 09:32:47 +0200

    save

--- Programming/JAVASCRIPT/Playground/Morpion/js.js
+++ Programming/JAVASCRIPT/Playground/Morpion/js.js
@@ -20,6 +20,18 @@
 const grid = document.querySelector(".grid");
 console.log(grid);
 
+const case1 = document.querySelector(".case1");
+const case2 = document.querySelector(".case2");
+const case3 = document.querySelector(".case3");
+const case4 = document.querySelector(".case4");
+const case5 = document.querySelector(".case5");
+const case6 = document.querySelector(".case6");
+const case7 = document.querySelector(".case7");
+const case8 = document.querySelector(".case8");
+const case9 = document.querySelector(".case9");
+
+const cross = document
+
 
 
 let playerOne = '0';
