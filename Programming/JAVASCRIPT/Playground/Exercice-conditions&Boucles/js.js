f7e11d90b959d39759e4200d9db2d9920d6b2080
RomAFCI <adnot.rom@gmail.com>
2025-06-06 11:21:04 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-conditions&Boucles/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-conditions&Boucles/js.js
@@ -12,7 +12,6 @@ function ageRequired(years) {
 
 ageRequired(years);
 
-
 // let verif = verifAge(18);
 // console.log(verif);
 
@@ -29,7 +28,6 @@ ageRequired(years);
 //     console.log(mot);
 // }
 
-
 // BOUCLES :
 
 for (let i = 0; i <= 20; i += 2) console.log(i);
@@ -41,43 +39,96 @@ for (let i = 1; i <= 20; i++) {
 }
 
 // BOUCLES WHILE :
+// MIS EN COMMENTAIRES POUR EVITER CONFLIT
+// let secretNumber = 0;
 
-let secretNumber = 0;
+// while (secretNumber != 77) {
+//   secretNumber = prompt("Devine entre 1 et 100");
 
-while (secretNumber != 77) {
-  secretNumber = prompt("Devine entre 1 et 100");
+//   if (secretNumber < 77) {
+//     alert("C'est plus !");
+//   } else if (secretNumber > 77) {
+//     alert("C'est moins !");
+//   }
 
-  if (secretNumber < 77) {
-    alert("C'est plus !");
-  } else if (secretNumber > 77) {
-    alert("C'est moins !");
-  }
-
-  if (secretNumber == 77) {
-    alert("Bravo ! Vous avez deviné le bon nombre !");
-  }
-}
+//   if (secretNumber == 77) {
+//     alert("Bravo ! Vous avez deviné le bon nombre !");
+//   }
+// }
 
 // EXO FUNCTION MOIS / ANNÉE :
 
+// let janvier = 1;
+// let février = 2;
+// let mars = 3;
+// let avril = 4;
+// let mai = 5;
+// let juin = 6;
+// let juillet = 7;
+// let aout = 8;
+// let septembre = 9;
+// let octobre = 10;
+// let novembre = 11;
+// let decembre = 12;
 
-let janvier = 1 ;
-let février = 2 ;
-let mars = 3 ;
-let avril = 4 ;
-let mai = 5 ;
-let juin = 6 ;
-let juillet = 7 ;
-let aout = 8 ;
-let septembre = 9 ;
-let octobre = 10 ;
-let novembre = 11 ;
-let decembre = 12 ;
+let mois;
+let nombreDeJour;
+
+const bouton = document.querySelector("button");
+
+
+bouton.addEventListener("click",
+  () => {
+    calendar();
+  })
 
 
 
-function calendar () {
-  while (j <= 31){
-    j++
+function calendar() {
+  mois = parseInt(prompt("choisis un mois pour connaître le nombre de jour"));
+
+  switch (mois) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      nombreDeJour = 31;
+      break;
+
+    case 2:
+      nombreDeJour = "28 ou 29 selon l'année";
+      break;
+
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      nombreDeJour = 30;
+      break;
+
+    default:
+      nombreDeJour = "Mois invalide";
   }
+
+  alert(nombreDeJour);
 }
+
+
+
+
+
+// essai
+// function calendar () {
+
+//   }
+
+//   while (j <= 31){
+
+// j = prompt("choisis un mois pour connaître le nombre de jour");
+
+// if (mois =)
+
+//   }
