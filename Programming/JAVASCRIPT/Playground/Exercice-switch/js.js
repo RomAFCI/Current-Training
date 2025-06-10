576cbfc70d359090df9f9228f40388da3098dfe9
RomAFCI <adnot.rom@gmail.com>
2025-06-10 09:14:26 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-switch/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-switch/js.js
@@ -4,26 +4,26 @@ let jour = 1;
 
 switch (jour) {
   case 1:
-    console.log("Lundi, Spaghetti Bolognaise")
+    console.log("Lundi, Spaghetti Bolognaise");
     break;
 
-    case 2:
-    console.log("Mardi, Poulet Rôti")
+  case 2:
+    console.log("Mardi, Poulet Rôti");
     break;
 
-    case 3:
-    console.log("Mercredi, Poisson pané")
+  case 3:
+    console.log("Mercredi, Poisson pané");
     break;
 
-    case 4:
-    console.log("Jeudi, Salade composée")
+  case 4:
+    console.log("Jeudi, Salade composée");
     break;
 
-    case 5:
-    console.log("Vendredi, Pizza Margherita")
+  case 5:
+    console.log("Vendredi, Pizza Margherita");
     break;
 
   default:
-    console.log("Le restaurant n'est pas ouvert le week end")
+    console.log("Le restaurant n'est pas ouvert le week end");
     break;
 }
