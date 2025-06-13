c8f61d4222966ad37e5255925356cfcf98361949
RomAFCI <adnot.rom@gmail.com>
2025-06-13 16:02:43 +0200

    save

---
+++ Programming/JAVASCRIPT/Playground/Exercice-pendu/js.js
@@ -0,0 +1,24 @@
+let divBase = document.querySelector(".divBase");
+let divPoteau = document.querySelector(".divPoteau");
+let divPoutre = document.querySelector(".divPoutre");
+let divSquare = document.querySelector(".divSquare");
+let divCorde = document.querySelector(".divCorde");
+let divTete = document.querySelector(".divTete");
+let divCorps = document.querySelector(".divCorps");
+let divJambeGauche = document.querySelector(".divJambeGauche");
+let divJambeDroite = document.querySelector(".divJambeDroite");
+let divBrasGauche = document.querySelector(".divBrasGauche");
+let divBrasDroit = document.querySelector(".divBrasDroit");
+let divVisage = document.querySelector(".divVisage");
+
+let saisie = document.querySelector(".inputStyle");
+let envoi = document.querySelector(".btnStyle");
+
+let motSecret = document.querySelector(".motSecret");
+
+
+let answer = "trucmuche";
+
+console.log(answer[2]);
+
+console.log(answer.length);
