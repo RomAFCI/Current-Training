8986bdcd37401242c012ea33788a3b7a48055adf
RomAFCI <adnot.rom@gmail.com>
2025-06-16 09:48:48 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-pendu/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-pendu/js.js
@@ -16,9 +16,35 @@ let envoi = document.querySelector(".btnStyle");
 
 let motSecret = document.querySelector(".motSecret");
 
-
 let answer = "trucmuche";
+let goodLetters = [];
 
-console.log(answer[2]);
+// Verifier la longueur du mot en tableau peu importe sa taille avec lenght
+// console.log(answer[2]);
+// console.log(answer.length);
 
-console.log(answer.length);
+// NOTES:
+// preventDefault();  empêche le rechargement de la page
+// match(/^[a-zA-Z]$/) - Vérifie si c’est une lettre (a-z)
+
+envoi.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let texte = saisie.value;
+  saisie.value = "";
+
+  if (!texte.match(/^[a-zA-Z]$/)) {
+    motSecret.textContent = "Entre une seule lettre entre a et z !";
+    return;
+  }
+  if (answer.includes(texte)) {
+    motSecret.textContent = goodLetters;
+
+    if (!goodLetters.includes(texte)) {
+      goodLetters.push(texte);
+    }
+    console.log(goodLetters);
+  } else {
+    motSecret.textContent = `Essaye encore`;
+  }
+});
