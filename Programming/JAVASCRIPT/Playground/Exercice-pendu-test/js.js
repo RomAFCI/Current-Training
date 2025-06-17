18f1c7afcb994e453f2ffe8c92a2cda79ae26fee
RomAFCI <adnot.rom@gmail.com>
2025-06-17 12:00:32 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-pendu-test/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-pendu-test/js.js
@@ -53,45 +53,69 @@ const words = [
   "armure",
 ];
 
+let letter;
+
 const wordsRandom = Math.floor(Math.random() * words.length);
 const wordsFound = words[wordsRandom];
-const input = document.querySelector(".input");
-const goodLetters = [];
-const triedLetters = [];
-
 console.log(wordsFound);
+
+const tabWordsTransform = wordsFound.split("");
+console.log(tabWordsTransform);
+
+const input = document.querySelector(".input");
+
+// const goodLetters = [];
+// const triedLetters = [];
+// const error = 0;
+
+const announcement = document.querySelector(".announcement");
 
 // EVENT
 
-input.addEventListener("keydown", () => {
-  let text = input.value;
-  input.value = "";
+input.addEventListener("keypress", (event) => {
+  letter = event.key;
+  
+  console.log(letter);
+  
+  if (!letter.match(/^[a-zA-Z]$/)) {
+    announcement.textContent = "écrit une lettre entre a et z";
 
-  console.log((input.value = text));
-
-  if ((text = wordsFound)) {
-    console.log("bravo!");
+    return;
   }
+
+  searchGoodLetters();
 });
 
-// FUNCTION 
+// FUNCTION
 
-// 💡❔ Spread operator ❔💡
+function searchGoodLetters() {
 
-function searchGoodLetters () {
+for (let i = 0; i < tabWordsTransform.length; i++) {
+   
+if (letter == [i]){
+
+}
+console.log([i]);
 
 }
 
+    
+ 
+}
+//   })
+
+// }
+
 // TABLEAU
 
+// words.forEach (function (words){
+//   console.log(words);
 
-words.forEach (function (words){
-  console.log(words);
-  
-})
+// })
 
 // 📓 NOTES:
 
 // Verifier la longueur du mot en tableau peu importe sa taille avec lenght
 // console.log(answer[2]);
 // console.log(answer.length);
+// 💡❔ Spread operator ❔💡
