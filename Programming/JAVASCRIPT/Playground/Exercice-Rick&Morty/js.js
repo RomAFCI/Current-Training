a0eb9401a49b8cb9109a21b729b359d95913bcb5
RomAFCI <adnot.rom@gmail.com>
2025-06-19 14:11:35 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-Rick&Morty/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-Rick&Morty/js.js
@@ -7,25 +7,27 @@
 let body = document.querySelector("body");
 let cards = document.querySelector(".cards");
 
-// API
-//   Récupére l'API en ligne de Rick&Morty.
-fetch("https://rickandmortyapi.com/api/character")
-  .then((response) => response.json())
-  .then((data) => {
-    // data.results contient un tableau de personnages
-    data.results.forEach((perso) => {
-      // Création d'élément "div" en HTML
-      let div = document.createElement("div");
-      body.appendChild(div);
+for (let page = 1; p <= data.info.pages; page++) {
+  // API
+  //   Récupére l'API en ligne de Rick&Morty.
+  fetch("https://rickandmortyapi.com/api/character?page=${page})")
+    .then((response) => response.json())
+    .then((data) => {
+      // data.results contient un tableau de personnages
+      data.results.forEach((perso) => {
+        // Création d'élément "div" en HTML
+        let div = document.createElement("div");
 
-    //   div.appendChild(cards);
+        div.classList.add("cards");
+        body.appendChild(div);
 
-      let p = document.createElement("p");
-      p.textContent = perso.name;
-      body.appendChild(p);
+        let p = document.createElement("p");
+        p.textContent = perso.name;
+        div.appendChild(p);
 
-      let img = document.createElement("img");
-      img.src = perso.image;
-      body.appendChild(img);
+        let img = document.createElement("img");
+        img.src = perso.image;
+        div.appendChild(img);
+      });
     });
-  });
+}
