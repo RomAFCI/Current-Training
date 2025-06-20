c3a72c8c4d22b47b58d455757d7850dcebe2d428
RomAFCI <adnot.rom@gmail.com>
2025-06-20 13:59:58 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exerice-map/js.js
+++ Programming/JAVASCRIPT/Playground/Exerice-map/js.js
@@ -1,33 +1,32 @@
-var map = L.map('map', {
-    center: [47, 2],
-    zoom: 6
+var map = L.map("map", {
+  center: [47, 2],
+  zoom: 6,
 });
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-
 
 console.log(map);
 
+let lattitude;
+let longitude;
+let city;
 
+map.addEventListener("click", (e) => {
+  lattitude = e.latlng.lat;
+  longitude = e.latlng.lng;
 
+  console.log(lattitude);
+  console.log(longitude);
+});
 
-let lattitude = map._lastCenter.lat;
-let longitude = map._lastCenter.lng;
-console.log(lattitude);
-console.log(longitude);
-
-
-
-map.addEventListener("click",() => {
-lattitude = map.latLng.lat;
-longitude = map.latLng.lng;
-console.log(lattitude);
-console.log(longitude);
-
-
-})
-
-
+fetch(
+  "https://nominatim.openstreetmap.org/reverse?lat=<value>&lon=<value>&<params>"
+)
+  .then((response) => response.json)
+  .then((data) => {
+    console.log(data);
+  });
