f227e3dacc72ab58f5d060b8fd4bb45713d2b850
RomAFCI <adnot.rom@gmail.com>
2025-06-24 14:31:50 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-mockapi/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-mockapi/js.js
@@ -1,10 +1,27 @@
 let form = document.querySelector("form");
-let text = document.querySelector(".text");
+
+let prenom = document.querySelector(".prenom");
+let nom = document.querySelector(".nom");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+  event.preventDefault();
+  console.log(prenom.value);
+  console.log(nom.value);
 
-    console.log(text.value);
-    
-})
-
+  fetch(`https://685a93639f6ef9611156f49a.mockapi.io/form-identity`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prenom: prenom.value,
+      nom: nom.value,
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      alert(`Utilisateur créé avec succès : ${JSON.stringify(data)}`);
+    });
+});
