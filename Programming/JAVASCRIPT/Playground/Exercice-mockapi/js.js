0101c52a1fc9b6faff21d04a7e62e10c8ba9be5f
RomAFCI <adnot.rom@gmail.com>
2025-06-24 11:54:03 +0200

    save

---
+++ Programming/JAVASCRIPT/Playground/Exercice-mockapi/js.js
@@ -0,0 +1,12 @@
+let form = document.querySelector("form");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    console.log(value);
+    
+})
