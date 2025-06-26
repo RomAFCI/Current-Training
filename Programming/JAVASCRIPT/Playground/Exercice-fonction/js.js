c5eecd4fcc0666adff7c065acd31c41a04766630
RomAFCI <adnot.rom@gmail.com>
2025-06-26 08:34:35 +0200

    save

---
+++ Programming/JAVASCRIPT/Playground/Exercice-fonction/js.js
@@ -0,0 +1,10 @@
+// Exercice 2 : Fusion de tableaux
+
+// Écrivez une fonction `mergeArrays` qui prend deux tableaux en entrée et retourne
+// un nouveau tableau qui est la fusion des deux tableaux d'entrée.
+
+let tab1 = [1, 2, 3, 4];
+let tab2 = [5, 6, 7, 8];
+let tab3 = tab1 + tab2;
+
+console.log(tab3);
