2bfdff3c0815cc6252e0efb7c6da20dc06bef8bb
RomAFCI <adnot.rom@gmail.com>
2025-06-26 14:35:02 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-motDePasse/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-motDePasse/js.js
@@ -1,13 +1,44 @@
 let range = document.querySelector(".range");
 let info = document.querySelector(".info");
 
+let btnSubmit = document.querySelector(".btnSubmit")
+
 let min = document.querySelector(".min");
 let maj = document.querySelector(".maj");
 let nbr = document.querySelector(".nbr");
-let car = document.querySelector(".car");
+let sym = document.querySelector(".sym");
 
 range.addEventListener("input", () => {
   console.log(range.value);
 
   info.textContent = range.value;
 });
+
+function chooseLetterNumberSymbol() {
+  min.addEventListener("click", () => {
+    min.style.backgroundColor = "white";
+
+  });
+
+  maj.addEventListener("click", () => {
+    maj.style.backgroundColor = "white";
+  });
+
+  nbr.addEventListener("click", () => {
+    nbr.style.backgroundColor = "white";
+  });
+
+  sym.addEventListener("click", () => {
+    sym.style.backgroundColor = "white";
+  });
+}
+
+chooseLetterNumberSymbol();
+
+btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+
+})
+
+
