c26ab50f851ef997a62511f93c0d4f102030e313
RomAFCI <adnot.rom@gmail.com>
2025-06-27 11:45:29 +0200

    save

--- Programming/JAVASCRIPT/Playground/Exercice-motDePasse/js.js
+++ Programming/JAVASCRIPT/Playground/Exercice-motDePasse/js.js
@@ -11,60 +11,9 @@ let sym = document.querySelector(".sym");
 
 let dataTab = [];
 let minTab = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+  "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 let majTab = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 ];
 let nbrTab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let symTab = ["!", "@", "#", "$", "%"];
@@ -118,6 +67,8 @@ btnSubmit.addEventListener("click", (e) => {
     let letterPassword = Math.floor(Math.random() * dataTab.length);
     passwordCreated += dataTab[letterPassword];
   }
+
+  // if ? -> si le mot de passe ne comporte pas les caractères demandé ⁉️
 
   console.log(passwordCreated);
 });
