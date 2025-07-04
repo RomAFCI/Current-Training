06041e83ef08c5e68bc39744776e34cf8efe1f9e
RomAFCI <adnot.rom@gmail.com>
2025-07-04 14:12:26 +0200

    save

---
+++ Ressources/php.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$dbname = 'rpg';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    // Active les erreurs PDO en exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie !";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+
+
+
+?>
